481f87362aaceb9c0f3b9d51bf0d59ac51d0762b
JJH <2022113621@dgu.ac.kr>
2025-04-01 23:33:57 +0900

    2025-04-01 개인과제5번 5번

---
+++ Info.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Info {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("이름을 입력하세요: ");
+		String name = sc.nextLine();
+		
+		System.out.print("학번을 입력하세요: ");
+		String studentId = sc.nextLine();
+		
+		System.out.print("학과를 입력하세요: ");
+		String department = sc.nextLine();
+		
+		System.out.println("\n<출력>");
+		System.out.println("이름: " + name);
+		System.out.println("학번: " + studentId);
+		System.out.println("학과: " + department);
+		
+		sc.close();
+	}
+
+}
